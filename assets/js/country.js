687ab13db7e2ae989fc35d96b883a943ab32f70e
shikharP <shikhar625apl@gmail.com>
2022-07-11 18:13:29 +0530

    code updated for verify form UI and api changes

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,30 +1,41 @@
-const countryList = [
 
-    "Australia",
-    "India",
-    "USA"
-
-];
+const initialize = async () => {
 
 
+    //API REQUEST 
+    fetch('http://minakycservicedev-env.eba-zmicm36h.us-east-1.elasticbeanstalk.com/KYCService/getCountryCodes/', {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+
+        .then(response => response.json())
+        .then(response => {
+            const body = response
+            const responseKey = Object.keys(body)
+            const responseValue = Object.values(body)
+
+            // 
+            responseValue.map((country, index) => {
+                const option = document.createElement("option")
+                option.innerHTML = country
+                option.value = responseKey[index]
+
+                const li1 = document.body.appendChild(option)
+                dropDown.appendChild(li1)
+                li1.classList.add('list-group-item', 'bg-light')
+            })
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+initialize()
 
 const dropDown = document.getElementById("dropDown")
-
-
-countryList.map((country, index) => {
-
-    //mapCountry.innerHTML = country
-
-    const option = document.createElement("option")
-    option.innerHTML = country
-    option.value = countryList[index]
-    const li1 = document.body.appendChild(option)
-    dropDown.appendChild(li1)
-    li1.classList.add('list-group-item', 'bg-light')
-
-
-
-})
 
 dropDown.onchange = (e) => {
     sessionStorage.setItem("countySelected", e.target.value)
