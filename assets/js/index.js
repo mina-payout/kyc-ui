92cc4da5c01138a9b7b7c67982cbb187e3423fdd
shikharP <shikhar625apl@gmail.com>
2022-07-25 17:42:25 +0530

    code updated for email , public key and trulio embed id tested and working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,30 @@
-
-
-
 (() => {
-  'use strict'
+  'use strict';
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-  const continueBtn = document.getElementById("continue-btn")
+  const forms = document.querySelectorAll('.needs-validation');
+  const continueBtn = document.getElementById('continue-btn');
+  const indexForm = document.forms['indexForm'];
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
 
-      form.classList.add('was-validated')
-
-      continueBtn.classList.remove('btn-primary')
-      continueBtn.classList.add('btn-dark')
-    }, false)
-  })
-
-})()
+        const email = document.indexForm.email.value;
+        const publicKey = document.indexForm.publicKey.value;
+        window.sessionStorage.setItem('userEmail', email);
+        window.sessionStorage.setItem('userPublicKey', publicKey);
+        form.classList.add('was-validated');
+        continueBtn.classList.remove('btn-primary');
+        continueBtn.classList.add('btn-dark');
+      },
+      false
+    );
+  });
+})();
